aac590348146500df9d602451e5206fdf64e0455
Joseph Lisee <jlisee@gmail.com>
2014-04-19 19:00:59 -0400

    First pass at a moderncv based LaTeX resume
    The big changes done in this pass:
    
     - Replaced all boiler plate with actual resume content
     - Organized my jobs into an experience list with sections
     - Changed dates to be ISO format based, and properly formatted them in
       the template
     - Handled escaping latex special characters in text

--- resugen/main.py
+++ resugen/main.py
@@ -8,14 +8,18 @@ __author__ = 'Joseph Lisee'
 __email__ = 'jlisee@gmail.com'
 
 # Python Imports
+import datetime
 import os
+import re
 import sys
-for p in sys.path:
-    print '  ',p
+
 # Library Imports
 import yaml
 import markupsafe
 import jinja2
+
+DATE_Y_M = re.compile('^\d{4}-\d{2}$')
+DATE_Y_M_D = re.compile('^\d{4}-\d{2}-\d{2}$')
 
 def stripext(value):
     """
@@ -29,9 +33,62 @@ def brace(value):
 
       {value}
 
-    Deal with the fact that: {{{dict.key}}} doesn't work.
+    Deal with the fact that: {{{dict.key}}} doesn't work. It will also
+    escape any latex special characters that are in the content.
     """
-    return '{' + value + '}'
+    return '{' + latex_escape(str(value)) + '}'
+
+def dateyear(value):
+    """
+    Return the year of the date.
+    """
+    date = obj2date(value)
+
+    if date:
+        return date.strftime('%Y')
+    else:
+        return value
+
+def obj2date(value):
+    """
+    If the object is not a date time, it attempts to convert it.
+    """
+    if not (isinstance(value, datetime.datetime) or
+            isinstance(value, datetime.date)):
+        if DATE_Y_M.match(value):
+            return datetime.datetime.strptime(value, '%Y-%m')
+        if DATE_Y_M_D.match(value):
+            return datetime.datetime.strptime(value, '%Y-%m-%d')
+        else:
+            return None
+
+    return value
+
+# Helper table for latex escape
+LATEX_ESCAPES = {
+    '&' : r'\&',
+    '%' : r'\%',
+    '$' : r'\$',
+    '#' : r'\#',
+    '_' : r'\letterunderscore{}',
+    '{' : r'\letteropenbrace{}',
+    '}' : r'\letterclosebrace{}',
+    '~' : r'\lettertilde{}',
+    '^' : r'\letterhat{}',
+    '\\': r'\letterbackslash{}',
+    '>' : r'\textgreater',
+    '<' : r'\lessthan',
+}
+
+
+def latex_escape(value):
+    """
+    Escapes all the special characters in latex strings.  This is a
+    simple replace routine based on the LATEX_ESCAPES dict.
+    """
+
+    return "".join([LATEX_ESCAPES.get(char, char) for char in value])
+
 
 def main(argv = None):
     # Process args
@@ -40,6 +97,10 @@ def main(argv = None):
 
     # Data to use in templating
     data = yaml.load(open(argv[1]))
+
+    # TODO: make this an escaping an option
+    #for d in data:
+
 
     # Our actual template source
     template_path = argv[2]
@@ -50,7 +111,9 @@ def main(argv = None):
     env = jinja2.Environment(loader = loader)
     env.filters.update({
         'stripext' : stripext,
-        'brace' : brace
+        'br' : brace,
+        'dateyear' : dateyear,
+        'lesc' : latex_escape,
         })
 
     template = env.get_template(template_path)
