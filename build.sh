a668ef86afba675983eee8d090f1aee375f792b9
Joseph Lisee <jlisee@gmail.com>
2014-05-03 22:03:34 -0400

    Add the ability to include a private "personal.yaml" file in the build
    This file is merged into the main resume.yaml file and allows for inclusion
    of things like a phone number and street address without checking them in.

--- build.sh
+++ build.sh
@@ -26,6 +26,14 @@ cp $DEPS_DIR/latex-makefile/Makefile $BUILD_DIR
 # Move our resources into the build director
 cp $ROOT_DIR/resources/* $BUILD_DIR
 
+# Add in extra args if the personal file exists
+EXTRA_FILE=$ROOT_DIR/personal.yaml
+if [ -f $EXTRA_FILE ]; then
+    EXTRA_ARGS="--extra $EXTRA_FILE"
+else
+    EXTRA_ARGS=""
+fi
+
 # Generate our latex file
 export PYTHONPATH=$ROOT_DIR:$DEPS_DIR/MarkupSafe:$DEPS_DIR/PyYAML/lib:$DEPS_DIR/jinja2:$PYTHONPATH
 
@@ -33,7 +41,8 @@ python -m resugen.main \
     --extension tex \
     $ROOT_DIR/resume.yaml \
     $ROOT_DIR/latex/template.jinja2 \
-    $BUILD_DIR
+    $BUILD_DIR \
+    $EXTRA_ARGS
 
 
 # Build the result
